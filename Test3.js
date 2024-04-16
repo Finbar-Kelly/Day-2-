a818715f35ac07abbc08906fd5fdb9ade815c894
Finbar-Kelly <167078766+Finbar-Kelly@users.noreply.github.com>
2024-04-16 09:19:09 +0000

    Cuboid success

--- Test3.js
+++ Test3.js
@@ -2,9 +2,19 @@ const readline = require("readline-sync");
 let width = readline.question(`What is the Width (Cm)`);
 let length = readline.question("What is the Length (Cm)");
 let height = readline.question("What is the Height (Cm)");
-let surfaceAreaOfCube = ( 2 * (length * width + length *  + width * height));
+
+let face1 = width * height 
+let face2 = width * length
+let face3 = height * length
+
+let surfaceAreaOfCube = ( 2 * (face1 + face2 + face3));
+
+
+
+
+
 let side;
 let volumeOfCube = (length * width * height);
-console.log(`The Volume of the cube is ${volumeOfCube}Cm`);
-console.log(`The Surface area of the cube is ${surfaceAreaOfCube}Cm`);
+console.log(`The Volume of the cube is ${volumeOfCube}Cm^3`);
+console.log(`The Surface area of the cube is ${surfaceAreaOfCube}Cm^2`);
 
