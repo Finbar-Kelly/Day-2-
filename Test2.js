acf2d3e9cbdb054197b62376090e567f23756538
Finbar-Kelly <167078766+Finbar-Kelly@users.noreply.github.com>
2024-04-16 09:01:18 +0000

    Test2 dog age works!

--- Test2.js
+++ Test2.js
@@ -1,9 +1,12 @@
 const readline = require("readline-sync");
 let dogAgeHumanYear = readline.question(`What is the dog age in Human Years?`);
-let dogAgeDogYears = 0;
+let dogAgeDogYears
 if (dogAgeHumanYear <= 2){
     dogAgeDogYears = 10.5 * dogAgeHumanYear;
     console.log(`The Dog age in Dog Years is ${dogAgeDogYears}`);
 }
-
+else {
+    dogAgeDogYears = 21 + (dogAgeHumanYear * 4)
+    console.log(`The Dog age in Dog Years is ${dogAgeDogYears}`);
+}
 //console.log(`The Dog age in Dog years is ${}`);
